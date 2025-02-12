c35a7dd9c293a1c1055aef91df4ded1da76de299
Rakka <rakka.sol@pm.me>
2025-02-13 01:04:02 +0400

    chore: deploy and verify devnet

--- sh/deployDevnet.sh
+++ sh/deployDevnet.sh
@@ -10,5 +10,9 @@ solana-verify build --library-name $LIBRARY_NAME -u $DEVNET_NETWORK_URL
 solana program deploy -u $DEVNET_NETWORK_URL target/deploy/$LIBRARY_NAME.so --with-compute-unit-price 50000 --max-sign-attempts 100 --use-rpc
 
 # To verify the contract on the devnet
+SVB_DOCKER_MEMORY_LIMIT=2g SVB_DOCKER_CPU_LIMIT=2
 solana-verify get-program-hash -u $DEVNET_NETWORK_URL $OMNIPAIR_PROGRAM_ID
 solana-verify verify-from-image -e target/deploy/$LIBRARY_NAME.so -i ellipsislabs/hello_world_verifiable_build:latest -p $OMNIPAIR_PROGRAM_ID
+solana-verify verify-from-repo -u $DEVNET_NETWORK_URL --program-id $OMNIPAIR_PROGRAM_ID https://github.com/$REPO_PATH --commit-hash $COMMIT_HASH --library-name $PROGRAM_LIB_NAME --mount-path $MOUNT_PATH
+solana-verify submit-job --program-id $OMNIPAIR_PROGRAM_ID --uploader 31rZbBENnRBmjmUxTkCT1LHp59EMmdmTgmooUyaNh3Gh --url $DEVNET_NETWORK_URL
+solana-verify verify-from-repo --program-id $OMNIPAIR_PROGRAM_ID https://github.com/$REPO_PATH --library-name $PROGRAM_LIB_NAME -u $DEVNET_NETWORK_URL
