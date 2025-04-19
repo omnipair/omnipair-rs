2ee3f192ae97937bf0ab267b0514f5287f16396e
Muhammed <mselrakabawi@gmail.com>
2025-04-19 11:34:31 +0400

    chore: clean up

--- programs/omnipair/src/instructions/mod.rs
+++ programs/omnipair/src/instructions/mod.rs
@@ -1,5 +1,3 @@
-pub mod pair_adjust_collateral;
-pub mod pair_adjust_debt;
 pub mod pair_initialize;
 pub mod pair_add_liquidity;
 pub mod pair_remove_liquidity;
@@ -7,8 +5,6 @@ pub mod pair_swap;
 pub mod rate_model_create;
 pub mod commons;
 
-pub use pair_adjust_collateral::*;
-pub use pair_adjust_debt::*;
 pub use pair_initialize::*;
 pub use pair_add_liquidity::*;
 pub use pair_remove_liquidity::*;
