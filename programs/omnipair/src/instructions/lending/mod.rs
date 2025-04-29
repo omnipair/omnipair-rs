17d7ec2f15193fdaa49dd1d2527eb30f8dddf341
Rakka <rakka.sol@pm.me>
2025-04-30 01:01:33 +0400

    chore: create rate model in pair initialize ixn

--- programs/omnipair/src/instructions/lending/mod.rs
+++ programs/omnipair/src/instructions/lending/mod.rs
@@ -1,9 +1,7 @@
 pub mod common;
-pub mod rate_model_create;
 pub mod pair_add_collateral;
 pub mod pair_remove_collateral;
 pub mod pair_borrow;
 pub mod pair_repay;
 
 pub use common::*;
-pub use rate_model_create::*;
--- programs/omnipair/src/instructions/mod.rs
+++ programs/omnipair/src/instructions/mod.rs
@@ -6,6 +6,5 @@ pub mod pair_initialize;
 pub use spot::*;
 pub use liquidity::*;
 pub use lending::common::*;
-pub use lending::rate_model_create::*;
 pub use lending::pair_add_collateral::*;
 pub use pair_initialize::*;
