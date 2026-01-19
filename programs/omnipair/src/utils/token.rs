d63cf38425b8a893e36a8964667d74fe112500ef
Muhammed <mselrakabawi@gmail.com>
2026-01-19 09:57:20 +0400

    audit-fix: Missing Zero-Amount Check When Minting and Burning Tokens

--- programs/omnipair/src/utils/token.rs
+++ programs/omnipair/src/utils/token.rs
@@ -188,6 +188,9 @@ pub fn token_mint_to<'a>(
     amount: u64,
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
+    if amount == 0 {
+        return Ok(());
+    }
     token_2022::mint_to(
         CpiContext::new_with_signer(
             token_program,
@@ -210,6 +213,9 @@ pub fn token_burn<'a>(
     amount: u64,
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
+    if amount == 0 {
+        return Ok(());
+    }
     token_2022::burn(
         CpiContext::new_with_signer(
             token_program.to_account_info(),
