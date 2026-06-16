8bb1bc742891f467e8db0d77d97ad0b500f111a5
Muhammed <muhammed@omnipair.fi>
2026-06-16 21:51:55 +0800

    test(v2): move market config tests

--- programs/omnipair-v2/src/state/config.rs
+++ programs/omnipair-v2/src/state/config.rs
@@ -85,3 +85,110 @@ impl MarketConfig {
 fn half_life_in_bounds(half_life_ms: u64) -> bool {
     (MIN_HALF_LIFE_MS..=MAX_HALF_LIFE_MS).contains(&half_life_ms)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::NAD;
+
+    fn valid_config() -> MarketConfig {
+        MarketConfig {
+            swap_fee_bps: 30,
+            operator_fee_bps: 1_000,
+            protocol_fee_bps: 0,
+            buffer_ratio_bps: 2_000,
+            fee_routing_k_nad: NAD,
+            ema_half_life_ms: 60_000,
+            directional_ema_half_life_ms: 60_000,
+            k_ema_half_life_ms: 60_000,
+            max_daily_borrow_bps: 2_000,
+            max_daily_withdraw_bps: 2_000,
+            spot_ema_divergence_bps: 1_000,
+            k_ema_drawdown_bps: 1_000,
+            recognized_collateral_cap_bps: 15_000,
+            market_health_min_bps: 11_000,
+            effective_debt_weight_min_bps: 10_000,
+            effective_debt_gamma_nad: NAD,
+            soft_borrow_enabled: false,
+            hedged_lp_enabled: true,
+            start_time: 0,
+        }
+    }
+
+    #[test]
+    fn market_config_rejects_soft_borrow_until_implemented() {
+        let mut config = valid_config();
+        config.soft_borrow_enabled = true;
+
+        let err = config.validate().unwrap_err();
+
+        assert_eq!(
+            err,
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+    }
+
+    #[test]
+    fn market_config_rejects_recognition_cap_below_health_floor() {
+        let mut config = valid_config();
+        config.recognized_collateral_cap_bps = 10_000;
+        config.market_health_min_bps = 11_000;
+
+        let err = config.validate().unwrap_err();
+
+        assert_eq!(
+            err,
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+    }
+
+    #[test]
+    fn market_config_rejects_inert_ema_half_lives() {
+        let mut config = valid_config();
+        config.ema_half_life_ms = 0;
+        assert_eq!(
+            config.validate().unwrap_err(),
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+
+        let mut config = valid_config();
+        config.directional_ema_half_life_ms = MIN_HALF_LIFE_MS - 1;
+        assert_eq!(
+            config.validate().unwrap_err(),
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+
+        let mut config = valid_config();
+        config.k_ema_half_life_ms = MAX_HALF_LIFE_MS + 1;
+        assert_eq!(
+            config.validate().unwrap_err(),
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+    }
+
+    #[test]
+    fn market_config_rejects_inert_fee_routing() {
+        let mut config = valid_config();
+        config.fee_routing_k_nad = 0;
+
+        let err = config.validate().unwrap_err();
+
+        assert_eq!(
+            err,
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+    }
+
+    #[test]
+    fn market_config_rejects_invalid_k_drawdown_limit() {
+        let mut config = valid_config();
+        config.k_ema_drawdown_bps = BPS_DENOMINATOR + 1;
+
+        let err = config.validate().unwrap_err();
+
+        assert_eq!(
+            err,
+            anchor_lang::prelude::error!(ErrorCode::InvalidMarketConfig)
+        );
+    }
+}
